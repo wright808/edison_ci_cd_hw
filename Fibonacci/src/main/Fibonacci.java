a186b98f53338bc6191630521b3e8398ffa618a7
Wright <223102052@ge.com>
2025-03-25 17:50:47 -0400

    Static checks

--- Fibonacci/src/main/Fibonacci.java
+++ Fibonacci/src/main/Fibonacci.java
@@ -1,17 +1,18 @@
-public class Fibonacci
-{
-	
-	public static long calculate(long i)
-	{	
-		if (i < 2)
+public class Fibonacci {
+	/**
+	 * Fibonacci calculate function
+	 * @param i Value to return fibonacci calculation for
+	 * @return Fibonacci value
+	 */
+	public static long calculate(final long i) {	
+		if (i < 2) {
 			return i;
-
+		}
 		long a = 0;
 		long b = 1;
 		long fib = 1;
 
-		for (long x = 2; x <= i; x++)
-		{
+		for (long x = 2; x <= i; x++) {
 			fib = a + b;
 			a = b;
 			b = fib;
